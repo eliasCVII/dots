668c50b13d2900f4877f2e2755010c8c578b4bcb
elias <elias.contreras@usach.cl>
2025-05-20 12:29:41 -0400

    updated nvim

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -21,53 +21,25 @@ return {
     words = { enabled = true },
   },
   keys = {
-    {
-      "gd",
-      function()
-        Snacks.picker.lsp_definitions()
-      end,
-      desc = "Goto Definition",
-    },
-    {
-      "gD",
-      function()
-        Snacks.picker.lsp_declarations()
-      end,
-      desc = "Goto Declaration",
-    },
-    {
-      "gr",
-      function()
-        Snacks.picker.lsp_references()
-      end,
-      nowait = true,
-      desc = "References",
-    },
-    {
-      "gI",
-      function()
-        Snacks.picker.lsp_implementations()
-      end,
-      desc = "Goto Implementation",
-    },
-    {
-      "gy",
-      function()
-        Snacks.picker.lsp_type_definitions()
-      end,
-      desc = "Goto T[y]pe Definition",
-    },
-    {
-      "<leader>cR",
-      function()
-        Snacks.rename.rename_file()
-      end,
-      desc = "Rename File",
-    },
-    { "<leader>sw", function() Snacks.picker.grep_word() end, desc = "Visual selection or word", mode = { "n", "x" } },
+    -- LSP
+    {"gd", function() Snacks.picker.lsp_definitions() end, desc = "Goto Definition",},
+    {"gD", function() Snacks.picker.lsp_declarations() end, desc = "Goto Declaration",},
+    {"gr", function() Snacks.picker.lsp_references() end, nowait = true, desc = "References",},
+    {"gI", function() Snacks.picker.lsp_implementations() end, desc = "Goto Implementation",},
+    {"gy", function() Snacks.picker.lsp_type_definitions() end, desc = "Goto T[y]pe Definition",},
+
+    -- Search
+    {"<leader>cR", function() Snacks.rename.rename_file() end, desc = "Rename File",},
+    {"<leader>sw", function() Snacks.picker.grep_word() end, desc = "Visual selection or word", mode = { "n", "x" },},
+    {"<leader>sd", function() Snacks.picker.diagnostics() end, desc = "Diagnostics",},
+    {"<leader>sD", function() Snacks.picker.diagnostics_buffer() end, desc = "Buffer Diagnostics",},
+    { "<leader>su", function() Snacks.picker.undo() end, desc = "Undo History" },
+
+    -- Other
+    { "<leader>sk", function() Snacks.picker.keymaps() end, desc = "Keymaps" },
+    { "<leader>ht", function() Snacks.picker.colorschemes() end, desc = "Colorschemes" },
   },
   init = function()
-
     vim.api.nvim_create_autocmd("User", {
       pattern = "VeryLazy",
       callback = function()
@@ -83,6 +55,7 @@ return {
         -- Create some toggle mappings
         Snacks.toggle.diagnostics():map("<F2>")
         Snacks.toggle.indent():map("<leader>ug")
+        Snacks.toggle.option("background", { off = "light", on = "dark", name = "Dark Background" }):map("<leader>ub")
         Snacks.toggle.dim():map("<leader>uD")
       end,
     })
--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,18 +1,13 @@
 return { -- finally a beautiful theme
-  {
-    "Verf/deepwhite.nvim",
-  },
-  {
-    "mcchrish/zenbones.nvim",
-    dependencies = "rktjmp/lush.nvim",
-  },
+  {"Verf/deepwhite.nvim"},
+  {"mcchrish/zenbones.nvim", dependencies = "rktjmp/lush.nvim",},
   {
     "mellow-theme/mellow.nvim",
     lazy = false,
     priority = 1000,
     config = function()
       vim.o.background = "dark"
-      vim.cmd.colorscheme("mellow")
+      vim.cmd.colorscheme("zenwritten")
     end,
   },
 }
