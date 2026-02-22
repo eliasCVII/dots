a07cafd6c63e8d125f6d5bf3ec6ed4a7038995d7
elias <elias.contreras@usach.cl>
2026-02-22 19:39:21 -0300

    nvim shit

--- .config/nvim/lua/plugins/smart-splits.lua
+++ .config/nvim/lua/plugins/smart-splits.lua
@@ -4,14 +4,6 @@ return {
   -- lazy = false,
   config = function()
     require("smart-splits").setup({
-      ignored_filetypes = {
-        "nofile",
-        "quickfix",
-        "prompt",
-      },
-      -- Ignored buffer types (only while resizing)
-      ignored_buftypes = { "NvimTree" },
-      -- the default number of lines/columns to resize by at a time
       default_amount = 5,
     })
     vim.keymap.set("n", "<A-h>", require("smart-splits").resize_left)
